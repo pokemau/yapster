70e03ea658128a59388ddcbfad8013a198c43aad
pokemau <rentillosa90@gmail.com>
2024-09-23 17:25:42 +0800

    add apps to INSTALLED_APPS

--- yapster/yapster/settings.py
+++ yapster/yapster/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chat.apps.ChatConfig',
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
