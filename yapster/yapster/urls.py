6185b6e6c245d91e4110ea755f58b5a31ef99bfb
JordanPooleEffect <zmtabinas@gmail.com>
2024-09-23 15:35:34 +0800

    (IN PROGRESS) LOGIN UI - ZED

--- yapster/yapster/urls.py
+++ yapster/yapster/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from yapster.views import login_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login_view, name='login'),
 ]
