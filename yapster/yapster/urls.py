84a32148f52b465357de83736402ccee29fae115
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2024-09-24 11:22:04 +0800

    added register ui

--- yapster/yapster/urls.py
+++ yapster/yapster/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from yapster.views import login_view
+from yapster.views import login_view, register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', login_view, name='login'),
+    path('register/', register_view, name='register'),
 ]
