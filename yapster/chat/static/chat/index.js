e9b1f23635821565743807e3a841d2e870bd5608
pokemau <rentillosa90@gmail.com>
2024-10-16 22:07:47 +0800

    add chat UI

---
+++ yapster/chat/static/chat/index.js
@@ -0,0 +1,7 @@
+const dots = document.querySelector("#dots");
+
+dots.addEventListener("click", () => {
+	const options = document.querySelector("#dots-options");
+
+	options.classList.toggle("hidden");
+});
