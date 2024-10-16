74b649813e3f1361a9e6c2059e126f08f2fae49b
pokemau <rentillosa90@gmail.com>
2024-10-16 11:03:36 +0800

    update register view

--- yapster/user/views.py
+++ yapster/user/views.py
@@ -7,55 +7,36 @@ from .models import YapsterUser
 
 
 def index_view(request):
-    """ (mau)
-    - diri nga view i check if logged in ang user or not
-    - if logged in ay redirect to home
-    - if not logged in kay redirect to login_view
-    """
+    if request.user.is_authenticated:
+        return redirect('chat')
     return redirect('login')
 
 def register_view(request):
-    """
-    TODO
-        - check if valid ang birthdate
-    """
     if request.method == 'POST':
         username = request.POST['username']
         fname = request.POST['fname']
         lname = request.POST['lname']
         password = request.POST['password']
         password2 = request.POST['password2']
-        email = request.POST['email']
-        gender = request.POST['gender']
-        birthday = datetime.strptime(request.POST['birthday'], "%Y-%m-%d")
 
         if password == password2:
-            if User.objects.filter(email=email).exists():
-                messages.info(request, 'Email exists')
-                return redirect('register')
-            elif User.objects.filter(username=username).exists():
+            if User.objects.filter(username=username).exists():
                 messages.info(request, 'Username exists')
                 return redirect('register')
             else:
                 user = User.objects.create_user(username=username,
-                                                email=email,
                                                 password=password)
                 user.first_name = fname
                 user.last_name = lname
                 user.save()
 
-                # login user
                 user_login = auth.authenticate(username=username,password=password)
                 auth.login(request, user_login)
 
                 user_ref = User.objects.get(username=username)
                 yapster_user = YapsterUser.objects.create(user=user_ref)
-                yapster_user.birthdate = birthday
-                yapster_user.gender = gender
                 yapster_user.save()
 
-                # TODO
-                # redirect to home_view / chat_view
                 return redirect('chat')
         else:
             messages.info(request, 'Passwords do not match')
