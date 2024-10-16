a063bf52aef5f1d578133b58020c0ba515fdabaf
pokemau <rentillosa90@gmail.com>
2024-10-16 11:06:36 +0800

    add logout user functionality

--- yapster/chat/urls.py
+++ yapster/chat/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.chat_view, name='chat'),
+    path('logout/', views.logout_user, name='logout'),
     path('test_chat/', views.test_chat_view, name='test_chat'),
 ]
--- yapster/chat/views.py
+++ yapster/chat/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, logout
+from django.contrib import messages
 from chat.models import Message, YapsterUser
 from user.models import User
 
 def chat_view(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
     return render(request, 'chat.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
 
 def test_chat_view(request):
     # check if logged in need verify na login si user
