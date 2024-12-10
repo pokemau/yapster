4fe23b5801cd317033447ee36201113741726516
pokemau <rentillosa90@gmail.com>
2024-12-11 01:13:27 +0800

    remove unused

--- yapster/friend/views.py
+++ yapster/friend/views.py
@@ -50,7 +50,6 @@ def accept_friend_request(request, request_id):
     receiver_friend_list, _ = FriendList.objects.get_or_create(user=request.user.yapsteruser)
     sender_friend_list, _ = FriendList.objects.get_or_create(user=friend_request.sender)
 
-    print('=======')
     print(type(friend_request))
     friend_request.accept()
     return redirect('friend:friend_requests')
